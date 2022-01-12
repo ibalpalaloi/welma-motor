4c1ecc3b7e6ae31bcbeaf0d81d55513b356efef2
Andipa <andipabp@gmail.com>
2022-01-12 11:12:02 +0800

    perbaikan

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -34,6 +34,12 @@
     <link rel="stylesheet" href="{{asset('assets/plugins/toastr/toastr.css')}}">
     <link rel="stylesheet" href="{{asset('assets/plugins/toastr/toastr.min.css')}}">
 
+    <style>
+        td {
+            white-space: normal !important; // To consider whitespace.
+            }
+    </style>
+
     @yield('header-scripts')
 
 
