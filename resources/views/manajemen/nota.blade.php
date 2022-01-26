671afbd56b1e8b7855ddfb8bbd56bab304af67d3
Andipa <andipabp@gmail.com>
2022-01-26 11:08:22 +0800

    perbaikan nota

--- resources/views/manajemen/nota.blade.php
+++ resources/views/manajemen/nota.blade.php
@@ -9,7 +9,6 @@
   
         @page{
             margin: 0px;
-            size: 9.5in 11in;
         }
 
         * {
@@ -18,11 +17,10 @@
 
         body{
 
-            margin-top: 15px;
-            margin-bottom: 15px;
-            margin-left: 15px;
-            margin-right: 25px;
-            margin-bottom: 15px;
+            margin: 10px 15px 10px;
+            padding-top: 6px;
+            padding-right: 15px;
+            font-size: 7pt;
         }
 
         h3{
@@ -30,17 +28,18 @@
             margin: 0px;
         }
 
-        table {
-            font-size: 8px;
+        #header {
+            position: fixed; 
+            left: 15px; top: 10px; right: 30px; height: 55px; 
+            text-align: center; 
+            
         }
 
+        .section-header{
 
-
-        tfoot tr td {
-
-            font-size: 8px;
+            margin-top: 88px;
         }
-
+ 
         .gray {
             background-color: lightgray
         }
@@ -50,11 +49,18 @@
             color: black;
         }
 
-        .title_information{
+        table{
 
-            font-size: 11px;
-
+            page-break-inside: avoid !important;
         }
+
+        .page-break {
+            page-break-after: always;
+        }
+
+
+        .pagenum:before { content: counter(page); }
+  
 
         .footer {
             position: fixed; 
@@ -126,81 +132,131 @@ return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
 
 <body>
 
-    <table width="100%" class="title_information">
-        <tr>
-            <td valign="top" width="50%">
-                <div class=""><h3>WELMA</h3></div>
-                <div class="">Bengkel Dan Variasi</div>
-                <div class="">Jl. Hangtuah No. 73 Telp 0451 428402, 081341114556</div>
-                <div class="">Palu 94118</div>
-                <hr width="95%" align="left" style=" border: 1px solid black; color:black;background-color:black" >
-            </td>
-            <td valign="top" align="right" width="50%">
-                <h3>Nota Penjualan</h3>
-            </td>
-        </tr>
-
-    </table>
-
-    <table width="100%" class="title_information">
-        <tr >
-            <td valign="top" align="left" width="50%">
-                <div class=""><strong style="padding-right: 26px;">Tanggal </strong> : {{ tgl_indo(date('Y-m-d', strtotime($riwayat_nota->tgl_nota))) }}</div>
-                <div class=""><strong style="padding-right: 20px;">Halaman </strong> : 1</div>
-
-            </td>
-            <td valign="top" align="left" width="50%">
-                <div class=""><strong>Kepada YTH</strong></div>
-                <div class=""><strong>{{strtoupper($riwayat_nota->nama_pembeli)}}</strong></div>        
-            </td>
-        </tr>
-
-    </table>
-
-    <br />
-
-    <table width="100%" style=" border: 1px solid black;border-collapse: collapse;">
-        <thead style="border: 1px solid black;border-collapse: collapse; ">
-            <tr height="15">
-                <th>No.</th>
-                <th>Kode Barang</th>
-                <th>Nama Barang</th>
-                <th>Satuan</th>
-                <th>Tipe</th>
-                <th>Merek</th>
-                <th>Jumlah</th>
-                <th>Harga</th>
-                <th>Total</th>
-            </tr>
-        </thead>
-        <tbody align="center">
-            @php
-                $total_harga = 0;
-            @endphp
-
-            @foreach ($riwayat_nota->riwayat_pesanan as $pesanan)
+    <div id="header">
+        <table width="100%" class="title_information">
             <tr>
-                <th scope="row">{{$loop->iteration}}</th>
-                @if ($pesanan->barang->jenis == 'barang')
-                    <td>{{$pesanan->kode_barang}}</td>
-                @else
-                    <td>Jasa</td>
-                @endif
-                <td align="left">{{$pesanan->nama_barang}}</td>
-                <td>{{$pesanan->barang->satuan}}</td>
-                <td >{{$pesanan->barang->tipe_barang}}</td>
-                <td>{{$pesanan->barang->merk}}</td>
-                <td>{{$pesanan->jumlah}}</td>
-                <td align="right">Rp. {{number_format($pesanan->harga,0,',','.')}}</td>
-                <td align="right">Rp. {{number_format($pesanan->jumlah * $pesanan->harga,0,',','.')}}</td>
+                <td valign="top" width="50%">
+                    <div class=""><h3>WELMA</h3></div>
+                    <div class="">Bengkel Dan Variasi</div>
+                    <div class="">Jl. Hangtuah No. 73 Telp 0451 428402, 081341114556</div>
+                    <div class="">Palu 94118</div>
+                    <hr width="95%" align="left" style=" border: 1px solid black; color:black;background-color:black" >
+                </td>
+                <td valign="top" align="right" width="50%">
+                    <h3>Nota Penjualan</h3>
+                </td>
             </tr>
-            @php
-                $total_harga += $pesanan->jumlah * $pesanan->harga
-            @endphp
-            @endforeach
-        </tbody>
+    
+        </table>
+    
+        <table width="100%" class="title_information">
+            <tr >
+                <td valign="top" align="left" width="50%">
+                    <div class=""><strong style="padding-right: 26px;">Tanggal </strong> : {{ tgl_indo(date('Y-m-d', strtotime($riwayat_nota->tgl_nota))) }}</div>
+                    <div class=""><strong style="padding-right: 20px;">Halaman </strong> : <span class="pagenum"></span></div>
+    
+                </td>
+                <td valign="top" align="left" width="50%">
+                    <div class=""><strong>Kepada YTH</strong></div>
+                    <div class=""><strong>{{strtoupper($riwayat_nota->nama_pembeli)}}</strong></div>        
+                </td>
+            </tr>
+    
+        </table>
+    </div>
 
-        <tfoot style="border: 1px solid black;">
+   <div class="section-header"></div>
+    <div id="content">
+        <table width="100%" style=" border: 1px solid black;border-collapse: collapse;">
+            <thead style="border: 1px solid black;border-collapse: collapse; ">
+                <tr height="15">
+                    <th>No.</th>
+                    <th>Kode Barang</th>
+                    <th>Nama Barang</th>
+                    <th>Satuan</th>
+                    <th>Tipe</th>
+                    <th>Merek</th>
+                    <th>Jumlah</th>
+                    <th>Harga</th>
+                    <th>Total</th>
+                </tr>
+            </thead>
+            <tbody align="center">
+                
+                @php
+                    $total_harga = 0;
+                @endphp
+    
+                @foreach ($riwayat_nota->riwayat_pesanan as $pesanan)
+                    <tr>
+                        <th scope="row">{{$loop->iteration}}</th>
+                        @if ($pesanan->barang->jenis == 'barang')
+                            <td>{{$pesanan->kode_barang}}</td>
+                        @else
+                            <td>Jasa</td>
+                        @endif
+                        <td align="left">{{$pesanan->nama_barang}}</td>
+                        <td>{{$pesanan->barang->satuan}}</td>
+                        <td style="white-space: normal;">{{$pesanan->barang->tipe_barang}}</td>
+                        <td>{{$pesanan->barang->merk}}</td>
+                        <td>{{$pesanan->jumlah}}</td>
+                        <td align="right">Rp. {{number_format($pesanan->harga,0,',','.')}}</td>
+                        <td align="right">Rp. {{number_format($pesanan->jumlah * $pesanan->harga,0,',','.')}}</td>
+                    </tr>
+                    @php
+                        $total_harga += $pesanan->jumlah * $pesanan->harga
+                    @endphp
+    
+            @if ($loop->iteration % 12 == 0)
+                </tbody>
+            </table>
+            <div class="page-break"></div>
+            <div class="section-header"></div>
+            <table width="100%" style=" border: 1px solid black;border-collapse: collapse;">
+                <thead style="border: 1px solid black;border-collapse: collapse; ">
+                    <tr height="15">
+                        <th>No.</th>
+                        <th>Kode Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Satuan</th>
+                        <th>Tipe</th>
+                        <th>Merek</th>
+                        <th>Jumlah</th>
+                        <th>Harga</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody align="center"> 
+                    @if ($loop->iteration % 12 != 0)
+                        <tr>
+                            <th scope="row">{{$loop->iteration}}</th>
+                            @if ($pesanan->barang->jenis == 'barang')
+                                <td>{{$pesanan->kode_barang}}</td>
+                            @else
+                                <td>Jasa</td>
+                            @endif
+                            <td align="left">{{$pesanan->nama_barang}}</td>
+                            <td>{{$pesanan->barang->satuan}}</td>
+                            <td style="white-space: normal;">{{$pesanan->barang->tipe_barang}}</td>
+                            <td>{{$pesanan->barang->merk}}</td>
+                            <td>{{$pesanan->jumlah}}</td>
+                            <td align="right">Rp. {{number_format($pesanan->harga,0,',','.')}}</td>
+                            <td align="right">Rp. {{number_format($pesanan->jumlah * $pesanan->harga,0,',','.')}}</td>
+                        </tr>
+                        @php
+                            $total_harga += $pesanan->jumlah * $pesanan->harga
+                        @endphp
+                    @endif
+            @endif
+    
+                @endforeach
+    
+            </tbody>
+        </table>
+    
+                
+    
+        <table width="100%" style="border: 1px solid black;">
             <tr  >
                 <td colspan="5" rowspan="5" valign="top">
                     <div class="">TERBILANG</div>
@@ -210,58 +266,65 @@ return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
                 <td align="right" colspan="2">Rp. {{number_format($total_harga,0,',','.')}}</td>
             </tr>
             <tr  >
-          
+        
                 <td align="left" colspan="2">PANJAR</td>
                 <td align="right" colspan="2">Rp. 0</td>
             </tr>
             
             <tr  >
-               
+            
                 <td align="left" colspan="2">POTONGAN</td>
                 <td align="right" colspan="2">Rp. 0</td>
             </tr>
             
             <tr  >
-             
+            
                 <td align="left" colspan="2">CHARGE</td>
                 <td align="right" colspan="2">Rp. 0</td>
             </tr>
             <tr  >
-             
+            
                 <td align="left" colspan="2">TOTAL HARGA</td>
                 <td align="right" colspan="2">Rp. {{number_format($total_harga,0,',','.')}}</td>
             </tr>
             
             
-        </tfoot>
-    </table>
-    <div class="" style="margin-top:15px;">
-
-        <table width="100%">
-            <tr valign="top">
-                <td align="left" style="width: 55%;">
-                    <div class="">*) Barang Yang Dibeli Sudah Diperiksa Dalam Kondisi Baik</div>
-                    <div class=""> *) Batas Waktu Retrun Barang 3 (Tiga) Hari Sejak Tanggal Pembelian</div>
-                    <div class="" style="margin-top : 15px;">Dicetak Pada Tangal : {{ tgl_indo(date('Y-m-d')) }}</div>
-                </td>
-                <td align="left" style="width: 15%;">
-                    @if ($riwayat_nota->montir != '-')
-                    <div class="" style="margin-bottom: 35px">Montir,</div>
-                    <div class="" style="text-decoration: underline;">{{strtoupper($riwayat_nota->montir)}}</div>
-                    @endif
-                </td>
-                <td align="left" style="width: 15%;">
-                    <div class="" style="margin-bottom: 35px">Penerima,</div>
-                    <div class="" style="text-decoration: underline;">{{strtoupper($riwayat_nota->nama_pembeli)}}</div>
-                </td>
-                <td align="left" style="width: 15%;">
-                    <div class="" style="margin-bottom: 35px">Mengetahui,</div>
-                    <div class="" style="text-decoration: underline;">{{strtoupper($riwayat_nota->user->nama)}}</div>
-                </td>
-            </tr>
-
         </table>
+     
+    
+       
+        <div class="" style="margin-top:15px;">
+    
+            <table width="100%">
+                <tr valign="top">
+                    <td align="left" style="width: 55%;">
+                        <div class="">*) Barang Yang Dibeli Sudah Diperiksa Dalam Kondisi Baik</div>
+                        <div class=""> *) Batas Waktu Retrun Barang 3 (Tiga) Hari Sejak Tanggal Pembelian</div>
+                        <div class="" style="margin-top : 15px;">Dicetak Pada Tangal : {{ tgl_indo(date('Y-m-d')) }}</div>
+                    </td>
+                    <td align="left" style="width: 15%;">
+                        @if ($riwayat_nota->montir)
+                            @if ($riwayat_nota->montir != '-')
+                            <div class="" style="margin-bottom: 35px">Montir,</div>
+                            <div class="" style="text-decoration: underline;">{{strtoupper($riwayat_nota->montir)}}</div>
+                            @endif
+                        @endif
+                    </td>
+                    <td align="left" style="width: 15%;">
+                        <div class="" style="margin-bottom: 35px">Penerima,</div>
+                        <div class="" style="text-decoration: underline;">{{strtoupper($riwayat_nota->nama_pembeli)}}</div>
+                    </td>
+                    <td align="left" style="width: 15%;">
+                        <div class="" style="margin-bottom: 35px">Mengetahui,</div>
+                        <div class="" style="text-decoration: underline;">{{strtoupper($riwayat_nota->user->nama)}}</div>
+                    </td>
+                </tr>
+    
+            </table>
+        </div>
     </div>
+ 
+
      
  
  
